a8416af5faa6d315f3f9b505e2a353953939d1d5
spez <128195573+spezontop@users.noreply.github.com>
2023-03-18 03:35:00 -0400

    Add files via upload

---
+++ To-do List/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace To_do_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.Submit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toDo = new System.Windows.Forms.Label();
+            this.Complete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(520, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(268, 394);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(12, 415);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(502, 23);
+            this.Submit.TabIndex = 1;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // toDo
+            // 
+            this.toDo.AutoSize = true;
+            this.toDo.Location = new System.Drawing.Point(12, 373);
+            this.toDo.Name = "toDo";
+            this.toDo.Size = new System.Drawing.Size(37, 13);
+            this.toDo.TabIndex = 3;
+            this.toDo.Text = "To-Do";
+            // 
+            // Complete
+            // 
+            this.Complete.Location = new System.Drawing.Point(520, 415);
+            this.Complete.Name = "Complete";
+            this.Complete.Size = new System.Drawing.Size(268, 23);
+            this.Complete.TabIndex = 4;
+            this.Complete.Text = "Complete";
+            this.Complete.UseVisualStyleBackColor = true;
+            this.Complete.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Complete);
+            this.Controls.Add(this.toDo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label toDo;
+        private System.Windows.Forms.Button Complete;
+    }
+}
+
---
+++ To-do List/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_do_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text.Trim();
+            if (!string.IsNullOrEmpty(text))
+            {
+                checkedListBox1.Items.Add(text);
+                textBox1.Text = string.Empty; 
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = checkedListBox1.SelectedIndex;
+            if (selectedIndex != -1)
+            {
+                checkedListBox1.Items.RemoveAt(selectedIndex);
+            }
+        }
+
+    }
+}
